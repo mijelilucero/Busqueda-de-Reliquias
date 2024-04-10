bd54b6c8b35b248f8228665c98e05337f78e2cdb
Mijeli <mlucerob@miumg.edu.gt>
2024-04-10 11:07:54 -0600

    Agregar archivos de proyecto.

---
+++ Battleship/Program.cs
@@ -0,0 +1,44 @@
+﻿int tamanioTablero = 10;
+
+string[] reliquias = { "S", "F", "ZZ", "MM", "EEE", "PPP", "RRRR", "BBBBB" };
+
+char[,] tableroOculto = inicializarTablero(tamanioTablero);
+char[,] tableroVisible = inicializarTablero(tamanioTablero);
+
+
+
+//for (int i = 0; i < tamanioTablero; i++)
+//{
+//    for (int j = 0; j < tamanioTablero; j++)
+//    {
+//        Console.Write("{0,3}", tableroOculto[i, j]);
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("\n\nTablero visible");
+//for (int i = 0; i < tamanioTablero; i++)
+//{
+//    for (int j = 0; j < tamanioTablero; j++)
+//    {
+//        Console.Write("{0,3}", tableroVisible[i, j]);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+static char[,] inicializarTablero(int tamanioTablero)
+{
+    char[,] tablero = new char[tamanioTablero, tamanioTablero];
+    
+    for (int i = 0; i < tamanioTablero; i++)
+    {
+        for (int j = 0; j < tamanioTablero; j++)
+        {
+            tablero[i, j] = 'O';
+        }
+    }
+
+    return tablero;
+}
